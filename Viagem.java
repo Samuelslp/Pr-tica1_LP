b0ea5d16943822610030bec04885c81f81da434c
Samuel Souza <127906251+Samuelslp@users.noreply.github.com>
2023-08-31 21:37:43 -0300

    Add files via upload

---
+++ Viagem.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Viagem {
+    public static void main(String[] args) {
+        double velocidade, tempo, distancia, kmLitro;
+        double consumo;
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite a velocidade: ");
+        velocidade = ler.nextDouble();
+        tempo= ler.nextDouble();
+        System.out.println("Digite Km/Litro: ");
+        kmLitro = ler.nextDouble();
+        distancia= velocidade * tempo;
+        consumo = distancia/kmLitro;
+        System.out.println("Consumo médio: " + consumo);
+    }
+}
